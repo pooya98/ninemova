76cab81aafdee0453d3a01c2e8ffc9ddcae1ad28
Seungwoo Kang <pooya98@naver.com>
2024-05-17 09:33:43 +0900

    ui(#10): LoginActivity 생성, LoginActivity UI 구현

--- app/src/main/java/com/ninemova/ui/LoginActivity.kt
+++ app/src/main/java/com/ninemova/ui/LoginActivity.kt
@@ -1,13 +1,24 @@
 package com.ninemova.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ninemova.R
+import com.ninemova.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent);
+            finish()
+        }
     }
 }
