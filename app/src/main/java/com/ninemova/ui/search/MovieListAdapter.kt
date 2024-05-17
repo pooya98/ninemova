e48b05c3201c4e409aa0373f291e122dd664214c
jaehan4707 <jahan4707@naver.com>
2024-05-17 10:18:59 +0900

    feat(#9): MovieListAdapter 구현

---
+++ app/src/main/java/com/ninemova/ui/search/MovieListAdapter.kt
@@ -0,0 +1,46 @@
+package com.ninemova.ui.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ninemova.databinding.ItemMovieBinding
+import com.ninemova.domain.data.Movie
+
+class MovieListAdapter : ListAdapter<Movie, MovieViewHolder>(diffUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding =
+            ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val diffUtil = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(
+                oldItem: Movie,
+                newItem: Movie,
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Movie,
+                newItem: Movie,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+class MovieViewHolder(private val binding: ItemMovieBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Movie) {
+        binding.imageUrl = item.posterPath
+    }
+}
