4fd8b45f0056eb83c1c1d8e309fb238158a652e0
lovejehan <jahan4707@naver.com>
2024-05-17 18:10:15 +0900

    feat(#19): DetailFragment, ViewModel, UiState 추가

---
+++ app/src/main/java/com/ninemova/ui/detail/DetailViewModel.kt
@@ -0,0 +1,35 @@
+package com.ninemova.ui.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ninemova.Network.RepositoryUtils
+import com.ninemova.domain.data.Movie
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class DetailViewModel : ViewModel() {
+
+    private val genreRepository = RepositoryUtils.genreRepository
+    private val _uiState = MutableStateFlow(DetailUiState())
+    val uiState: StateFlow<DetailUiState> = _uiState
+
+    fun setMovie(item: Movie) {
+        _uiState.update { state ->
+            state.copy(
+                movie = item
+            )
+        }
+        viewModelScope.launch {
+            genreRepository.getGenres().collectLatest { genres ->
+                _uiState.update { state ->
+                    state.copy(
+                        genres = genres
+                    )
+                }
+            }
+        }
+    }
+}
