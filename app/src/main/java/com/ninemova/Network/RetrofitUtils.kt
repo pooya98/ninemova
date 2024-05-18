60b84b4687342b24b861f1b6f9c38126842bb94f
jaehan4707 <jahan4707@naver.com>
2024-05-18 16:04:46 +0900

    feat(#24): YoutubeApi 구현

--- app/src/main/java/com/ninemova/Network/RetrofitUtils.kt
+++ app/src/main/java/com/ninemova/Network/RetrofitUtils.kt
@@ -2,6 +2,7 @@ package com.ninemova.Network
 
 import com.ninemova.Network.api.GenreApi
 import com.ninemova.Network.api.SearchApi
+import com.ninemova.Network.api.YoutubeApi
 import com.ninemova.core.NineMovaApplication
 import retrofit2.create
 
@@ -10,5 +11,6 @@ class RetrofitUtils {
     companion object {
         val searchApi: SearchApi = NineMovaApplication.retrofit.create()
         val genreApi: GenreApi = NineMovaApplication.retrofit.create()
+        val youtubeApi: YoutubeApi = NineMovaApplication.youtubeRetrofit.create()
     }
 }
