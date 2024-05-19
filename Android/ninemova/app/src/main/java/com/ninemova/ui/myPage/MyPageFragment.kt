fe0845f8684a4d14d117bd42560f9d2d5a23304a
pooya98 <pooya98@naver.com>
2024-05-19 20:06:01 +0900

    feat(#36): MyPageFragment에 ViewModel 적용

--- Android/ninemova/app/src/main/java/com/ninemova/ui/myPage/MyPageFragment.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/myPage/MyPageFragment.kt
@@ -1,36 +1,29 @@
 package com.ninemova.ui.myPage
 
-import android.graphics.Color
+import androidx.fragment.app.viewModels
 import com.ninemova.R
 import com.ninemova.databinding.FragmentMyPageBinding
 import com.ninemova.ui.base.BaseFragment
-import org.eazegraph.lib.charts.PieChart
-import org.eazegraph.lib.models.PieModel
-
 
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
 
-
+    private val myPageViewModel: MyPageViewModel by viewModels()
     override fun initView() {
+        with(binding) {
+            viewModel = myPageViewModel
+        }
 
-        val piechart_keyword: PieChart = binding.piechartKeyword
+        initUserTagList()
+        initPieChart()
+    }
 
-        piechart_keyword.addPieSlice(PieModel("R", 30F, Color.parseColor("#FFA726")))
-        piechart_keyword.addPieSlice(PieModel("Python", 40F, Color.parseColor("#66BB6A")))
-        piechart_keyword.addPieSlice(PieModel("C++", 30F, Color.parseColor("#EF5350")))
+    private fun initUserTagList() {
+        myPageViewModel.fetchUserTagResponse()
+    }
 
-        val piechart_genre: PieChart = binding.piechartGenre
-        piechart_genre.addPieSlice(PieModel("R", 30F, Color.parseColor("#FFA726")))
-        piechart_genre.addPieSlice(PieModel("Python", 40F, Color.parseColor("#66BB6A")))
-        piechart_genre.addPieSlice(PieModel("C++", 30F, Color.parseColor("#EF5350")))
-
-        val piechart_actor: PieChart = binding.piechartActor
-        piechart_actor.addPieSlice(PieModel("R", 30F, Color.parseColor("#FFA726")))
-        piechart_actor.addPieSlice(PieModel("Python", 40F, Color.parseColor("#66BB6A")))
-        piechart_actor.addPieSlice(PieModel("C++", 30F, Color.parseColor("#EF5350")))
-
-        piechart_keyword.startAnimation();
-        piechart_genre.startAnimation();
-        piechart_actor.startAnimation();
+    private fun initPieChart() {
+        myPageViewModel.fetchKeywordResponse()
+        myPageViewModel.fetchGenreResponse()
+        myPageViewModel.fetchActorResponse()
     }
 }
