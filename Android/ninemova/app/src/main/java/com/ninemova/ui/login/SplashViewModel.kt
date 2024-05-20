5de7d37e3a204177862614890202da46d0040b94
lovejehan <jahan4707@naver.com>
2024-05-20 16:54:39 +0900

    feat(#43): SplashViewModel 추가
    회원가입이 안된 경우 로그인 화면으로 이동, 된 경우 자동 로그인

---
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/login/SplashViewModel.kt
@@ -0,0 +1,30 @@
+package com.ninemova.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ninemova.Network.RepositoryUtils
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+
+class SplashViewModel :
+    ViewModel() {
+
+
+    private val _uiEvent = MutableSharedFlow<LoginViewEvent>()
+    val uiEvent: SharedFlow<LoginViewEvent> = _uiEvent
+    private val localDataStoreRepository = RepositoryUtils.localDataStoreRepository
+
+    init {
+        loadUserInfo()
+    }
+
+    private fun loadUserInfo() {
+        viewModelScope.launch {
+            val userId = localDataStoreRepository.getUserId()
+            if (userId != -1) {
+                _uiEvent.emit(LoginViewEvent.NavigateToMain)
+            }
+        }
+    }
+}
