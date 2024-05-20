e108986a41967eb33cfdcb4508ca175a81c10fcc
lovejehan <jahan4707@naver.com>
2024-05-20 16:55:46 +0900

    feat(#43): SplashScreen 추가, 애니메이션 적용, uiEvent collect

--- Android/ninemova/app/src/main/java/com/ninemova/ui/login/LoginActivity.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/login/LoginActivity.kt
@@ -1,20 +1,80 @@
 package com.ninemova.ui.login
 
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.activity.viewModels
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.NavHostFragment
+import com.ninemova.Network.RepositoryUtils
+import com.ninemova.Network.repositoryimpl.LocalDataStoreRepositoryImpl
 import com.ninemova.R
+import com.ninemova.core.dataStore
 import com.ninemova.databinding.ActivityLoginBinding
+import com.ninemova.ui.MainActivity
 import com.ninemova.ui.base.BaseActivity
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
 
+    private val viewModel: SplashViewModel by viewModels()
     private lateinit var navHostFragment: NavHostFragment
+    private lateinit var splashScreen: SplashScreen
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        RepositoryUtils.localDataStoreRepository =
+            LocalDataStoreRepositoryImpl(applicationContext.dataStore)
+        splashScreen = installSplashScreen()
+        startAnimation()
+        super.onCreate(savedInstanceState)
+    }
 
     override fun init() {
+        collectUiEvent()
         initNavigation()
     }
 
     private fun initNavigation() {
         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_login) as NavHostFragment
+    }
+
+    private fun collectUiEvent() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiEvent.collectLatest { event ->
+                    when (event) {
+                        is LoginViewEvent.NavigateToMain -> {
+                            startActivity(this@LoginActivity, MainActivity::class.java)
+                            finish()
+                        }
+
+                        else -> {}
+                    }
+                }
+            }
+        }
+    }
+
+    private fun startAnimation() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.view.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 1000
+            slideUp.doOnEnd { splashScreenView.remove() }
+            slideUp.start()
+        }
     }
 }
