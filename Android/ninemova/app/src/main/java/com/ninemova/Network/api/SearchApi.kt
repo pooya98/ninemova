b2a63680be19bed839f44f5d86ea3502a9505d60
jaehan4707 <jahan4707@naver.com>
2024-05-21 00:45:36 +0900

    feat(#47): getMovie api 구현

--- Android/ninemova/app/src/main/java/com/ninemova/Network/api/SearchApi.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/Network/api/SearchApi.kt
@@ -1,8 +1,10 @@
 package com.ninemova.Network.api
 
+import com.ninemova.Network.response.MovieResult
 import com.ninemova.Network.response.MoviesResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SearchApi {
@@ -14,4 +16,10 @@ interface SearchApi {
         @Query("page") page: Int,
         @Query("query") query: String,
     ): Response<MoviesResponse>
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovie(
+        @Path("movie_id") moveId: Int,
+        @Query("language") language: String,
+    ): Response<MovieResult>
 }
