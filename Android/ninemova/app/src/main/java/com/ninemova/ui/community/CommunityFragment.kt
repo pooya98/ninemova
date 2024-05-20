a0610facfc138360ce0f29d5322ed8f4e3059ac5
jaehan4707 <jahan4707@naver.com>
2024-05-21 00:59:27 +0900

    feat(#47): CommunityFragment GridLayout으로 변경, viewModel 바인딩

--- Android/ninemova/app/src/main/java/com/ninemova/ui/community/CommunityFragment.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/community/CommunityFragment.kt
@@ -1,29 +1,23 @@
 package com.ninemova.ui.community
 
-import com.google.android.flexbox.FlexDirection
-import com.google.android.flexbox.FlexWrap
-import com.google.android.flexbox.FlexboxLayoutManager
+import androidx.fragment.app.viewModels
 import com.ninemova.R
 import com.ninemova.databinding.FragmentCommunityBinding
-import com.ninemova.domain.data.Comment
 import com.ninemova.ui.adapter.CommentListAdapter
 import com.ninemova.ui.base.BaseFragment
 
 class CommunityFragment : BaseFragment<FragmentCommunityBinding>(R.layout.fragment_community) {
 
+    private val communityViewModel: CommunityViewModel by viewModels()
     private val commentListAdapter = CommentListAdapter()
     override fun initView() {
+        binding.viewModel = communityViewModel
         setRecyclerView()
     }
 
     private fun setRecyclerView() {
         with(binding.recyclerViewComment) {
             adapter = commentListAdapter
-            commentListAdapter.submitList(Comment.commentList.toMutableList())
-            layoutManager = FlexboxLayoutManager(context).apply {
-                flexWrap = FlexWrap.WRAP
-                flexDirection = FlexDirection.ROW
-            }
         }
     }
 }
