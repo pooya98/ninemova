0cdd337e8e1b8068c4f7a1fe2fcd8b03bc974f70
jaehan4707 <jahan4707@naver.com>
2024-05-21 00:34:43 +0900

    feat(#47): Comment 수정

--- Android/ninemova/app/src/main/java/com/ninemova/domain/data/Comment.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/domain/data/Comment.kt
@@ -2,48 +2,10 @@ package com.ninemova.domain.data
 
 data class Comment(
     val id: Int? = null,
-    val movieId: Int? = null,
+    val movieName: String? = null,
+    val posterPath: String? = null,
     val commentScore: Double? = null,
     val commentContent: String? = null,
+    val userId: Int? = null,
     val writer: String? = null,
-) {
-    companion object {
-        val commentList = listOf<Comment>(
-            Comment(
-                id = 0,
-                movieId = 0,
-                commentScore = 5.0,
-                commentContent = "명작영화",
-                writer = "방구석 전문가",
-            ),
-            Comment(
-                id = 1,
-                movieId = 0,
-                commentScore = 4.0,
-                commentContent = "명작영화1",
-                writer = "방구석 전문가",
-            ),
-            Comment(
-                id = 2,
-                movieId = 0,
-                commentScore = 3.0,
-                commentContent = "명작영화2",
-                writer = "방구석 전문가",
-            ),
-            Comment(
-                id = 3,
-                movieId = 0,
-                commentScore = 2.0,
-                commentContent = "명작영화3",
-                writer = "방구석 전문가",
-            ),
-            Comment(
-                id = 4,
-                movieId = 0,
-                commentScore = 1.0,
-                commentContent = "명작영화4",
-                writer = "방구석 전문가",
-            ),
-        )
-    }
-}
+)
