7024a788efa4e15bb05ad94fe85ff61762dfd173
lovejehan <jahan4707@naver.com>
2024-05-21 17:55:18 +0900

    feat(#59): safeArgs 추가

--- Android/ninemova/app/src/main/java/com/ninemova/ui/post/PostFragment.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/post/PostFragment.kt
@@ -1,10 +1,14 @@
 package com.ninemova.ui.post
 
+import androidx.navigation.fragment.navArgs
 import com.ninemova.R
 import com.ninemova.databinding.FragmentPostBinding
 import com.ninemova.ui.base.BaseFragment
 
 class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
+
+
+    private val args: PostFragmentArgs by navArgs()
 
     override fun initView() {
     }
