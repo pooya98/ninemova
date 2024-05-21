eec1515be1d1d2314c0bc21ed3632885e425516c
pooya98 <pooya98@naver.com>
2024-05-21 13:00:19 +0900

    feat(#50): TMDB Api 추가 (인기영화 리스트 가져오기, 상영중인 영화 리스트 가져오기)

--- Android/ninemova/app/src/main/java/com/ninemova/Network/api/SearchApi.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/Network/api/SearchApi.kt
@@ -2,6 +2,7 @@ package com.ninemova.Network.api
 
 import com.ninemova.Network.response.MovieResult
 import com.ninemova.Network.response.MoviesResponse
+import com.ninemova.domain.data.Movie
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -22,4 +23,16 @@ interface SearchApi {
         @Path("movie_id") moveId: Int,
         @Query("language") language: String,
     ): Response<MovieResult>
+
+    @GET("movie/now_playing")
+    suspend fun getNowPlayingMovies(
+        @Query("language") language: String,
+        @Query("page") page: Int,
+    ): Response<MoviesResponse>
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("language") language: String,
+        @Query("page") page: Int,
+    ): Response<MoviesResponse>
 }
