260106d838283d63a729a52bd1a4e83648a232d0
jaehan4707 <jahan4707@naver.com>
2024-05-22 00:23:49 +0900

    feat(#68): Reply, ReplyRequest,ReplyResponse 추가

---
+++ Android/ninemova/app/src/main/java/com/ninemova/domain/data/Reply.kt
@@ -0,0 +1,8 @@
+package com.ninemova.domain.data
+
+data class Reply(
+    val id: Int? = null,
+    val userNickName: String? = null,
+    val content: String? = null,
+    val userProfileImageUrl: String? = null,
+)
