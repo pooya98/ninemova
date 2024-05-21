3032e66dbf2f784991a5b5e3fcc8606b57d759ae
pooya98 <pooya98@naver.com>
2024-05-21 13:01:47 +0900

    feat(#50): MovieRepository 수정 (인기 영화 리스트 가져오기, 상영중인 영화 리스트 가져오기)

--- Android/ninemova/app/src/main/java/com/ninemova/Network/repository/MovieRepository.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/Network/repository/MovieRepository.kt
@@ -1,10 +1,16 @@
 package com.ninemova.Network.repository
 
 import com.ninemova.Network.request.SearchMovieRequest
+import com.ninemova.Network.request.SearchNowPlayingMoviesRequest
+import com.ninemova.Network.request.SearchPopularMoviesRequest
 import com.ninemova.domain.data.Movie
 import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
 
     suspend fun searchMovies(request: SearchMovieRequest): Flow<List<Movie>>
+
+    suspend fun searchNowPlayingMovies(request: SearchNowPlayingMoviesRequest): Flow<List<Movie>>
+
+    suspend fun searchPopularMovies(request: SearchPopularMoviesRequest): Flow<List<Movie>>
 }
--- Android/ninemova/app/src/main/java/com/ninemova/Network/repositoryimpl/MovieRepositoryImpl.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/Network/repositoryimpl/MovieRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.ninemova.Network.repositoryimpl
 import com.ninemova.Network.RetrofitUtils
 import com.ninemova.Network.repository.MovieRepository
 import com.ninemova.Network.request.SearchMovieRequest
+import com.ninemova.Network.request.SearchNowPlayingMoviesRequest
+import com.ninemova.Network.request.SearchPopularMoviesRequest
 import com.ninemova.domain.data.Movie
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -36,6 +38,66 @@ class MovieRepositoryImpl : MovieRepository {
                                 releaseDate = movieResponse.releaseDate,
                             )
                         },
+                    )
+                }
+            }.onFailure {
+                emit(emptyList())
+            }
+        }
+
+    override suspend fun searchNowPlayingMovies(request: SearchNowPlayingMoviesRequest): Flow<List<Movie>> =
+        flow {
+            runCatching {
+                api.getNowPlayingMovies(
+                    language = request.language ?: "ko",
+                    page = request.page ?: 1,
+                )
+            }.onSuccess {
+                it.body()?.let { response ->
+                    emit(
+                        response.results.filter { it.posterPath != null }.map { movieResponse ->
+                            Movie(
+                                id = movieResponse.id,
+                                title = movieResponse.title,
+                                genreIds = movieResponse.genreIds ?: listOf(),
+                                adult = movieResponse.adult,
+                                overView = movieResponse.overView,
+                                posterPath = movieResponse.posterPath ?: movieResponse.backdropPath,
+                                backdropPath = movieResponse.backdropPath
+                                    ?: movieResponse.posterPath,
+                                releaseDate = movieResponse.releaseDate,
+                            )
+                        },
+                    )
+                }
+            }.onFailure {
+                emit(emptyList())
+            }
+        }
+
+    override suspend fun searchPopularMovies(request: SearchPopularMoviesRequest): Flow<List<Movie>> =
+        flow {
+            runCatching {
+                api.getPopularMovies(
+                    language = request.language ?: "ko",
+                    page = request.page ?: 2,
+                )
+            }.onSuccess {
+                it.body()?.let { response ->
+                    emit(
+                        response.results.filter { it.posterPath != null }.map { movieResponse ->
+                            Movie(
+                                id = movieResponse.id,
+                                title = movieResponse.title,
+                                genreIds = movieResponse.genreIds ?: listOf(),
+                                adult = movieResponse.adult,
+                                overView = movieResponse.overView,
+                                posterPath = movieResponse.posterPath ?: movieResponse.backdropPath,
+                                backdropPath = movieResponse.backdropPath
+                                    ?: movieResponse.posterPath,
+                                releaseDate = movieResponse.releaseDate,
+                            )
+                        }.subList(0, 5),
                     )
                 }
             }.onFailure {
