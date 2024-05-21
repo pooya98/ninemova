51a1e4abc0491f9fccdea1c0251d32b552d8a13e
lovejehan <jahan4707@naver.com>
2024-05-21 17:53:04 +0900

    ui(#59): PostFragment UI 생성

---
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/post/PostFragment.kt
@@ -0,0 +1,12 @@
+package com.ninemova.ui.post
+
+import com.ninemova.R
+import com.ninemova.databinding.FragmentPostBinding
+import com.ninemova.ui.base.BaseFragment
+
+class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
+
+    override fun initView() {
+    }
+
+}
