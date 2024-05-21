7fdc153040872d774726c6d1873c3d178fba6d01
jaehan4707 <jahan4707@naver.com>
2024-05-22 00:30:10 +0900

    feat(#68): PostFragment 구현, reply data 연결

--- Android/ninemova/app/src/main/java/com/ninemova/ui/post/PostFragment.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/post/PostFragment.kt
@@ -1,19 +1,29 @@
 package com.ninemova.ui.post
 
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.ninemova.R
 import com.ninemova.databinding.FragmentPostBinding
+import com.ninemova.ui.adapter.ReplyListAdapter
 import com.ninemova.ui.base.BaseFragment
 
 class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
 
-
     private val args: PostFragmentArgs by navArgs()
+    private val postViewModel: PostViewModel by viewModels()
+    private val replyListAdapter = ReplyListAdapter()
 
     override fun initView() {
         with(binding) {
-            comment = args.comment
+            postViewModel.setComment(args.comment)
+            viewModel = postViewModel
         }
+        setRecyclerView()
     }
 
+    private fun setRecyclerView() {
+        with(binding.recyclerViewReply) {
+            adapter = replyListAdapter
+        }
+    }
 }
