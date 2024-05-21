c64f08b6e285b0bec4367c5d2cc78e4918f21c03
jaehan4707 <jahan4707@naver.com>
2024-05-22 00:25:25 +0900

    feat(#69): findUser API 구현

--- Android/ninemova/app/src/main/java/com/ninemova/Network/api/UserApi.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/Network/api/UserApi.kt
@@ -4,10 +4,15 @@ import com.ninemova.Network.request.SignUpRequest
 import com.ninemova.Network.response.UserResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface UserApi {
 
     @POST("user/signUp")
     suspend fun signUp(@Body request: SignUpRequest): Response<UserResponse>
+
+    @GET("user/{user_id}")
+    suspend fun findUser(@Path("user_id") userId: Int): Response<UserResponse>
 }
