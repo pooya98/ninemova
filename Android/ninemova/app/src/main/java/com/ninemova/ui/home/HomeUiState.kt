0916782fef20b948ffd4606781803217dc47239c
pooya98 <pooya98@naver.com>
2024-05-21 22:54:11 +0900

    feat(#58): HomeViewModel에 Comment Data 추가

--- Android/ninemova/app/src/main/java/com/ninemova/ui/home/HomeUiState.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/home/HomeUiState.kt
@@ -1,9 +1,11 @@
 package com.ninemova.ui.home
 
+import com.ninemova.domain.data.Comment
 import com.ninemova.domain.data.Movie
 
 data class HomeUiState(
     val query: String? = "",
     val popularMovies: List<Movie> = listOf(),
-    val nowPlayingMovies: List<Movie> = listOf()
+    val nowPlayingMovies: List<Movie> = listOf(),
+    val recentComments: List<Comment> = listOf()
 )
