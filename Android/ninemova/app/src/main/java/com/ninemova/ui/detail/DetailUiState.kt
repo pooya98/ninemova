96f95b1dd864c104e13bc075adbb0a11389406de
lovejehan <jahan4707@naver.com>
2024-05-22 15:27:45 +0900

    feat(#77): DetailUiState isLiked 파라미터 추가

--- Android/ninemova/app/src/main/java/com/ninemova/ui/detail/DetailUiState.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/detail/DetailUiState.kt
@@ -4,7 +4,8 @@ import com.ninemova.domain.data.Genre
 import com.ninemova.domain.data.Movie
 
 data class DetailUiState(
-    val movie: Movie? = null,
+    val movie: Movie = Movie(),
     val genres: List<Genre> = listOf(),
     val videoId: String? = null,
+    val isLiked: Boolean = false,
 )
