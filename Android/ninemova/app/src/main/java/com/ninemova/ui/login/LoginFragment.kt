9a05a79510421e89e1816550c1bd264afaee498d
jaehan4707 <jahan4707@naver.com>
2024-05-22 23:25:06 +0900

    ui(#87): 로그인 프래그먼트 UI 수정

--- Android/ninemova/app/src/main/java/com/ninemova/ui/login/LoginFragment.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/login/LoginFragment.kt
@@ -1,25 +1,19 @@
 package com.ninemova.ui.login
 
-import android.content.Intent
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.ninemova.R
 import com.ninemova.databinding.FragmentLoginBinding
-import com.ninemova.ui.MainActivity
 import com.ninemova.ui.base.BaseFragment
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
-    private val loginViewModel : LoginViewModel by viewModels()
+    private val loginViewModel: LoginViewModel by viewModels()
     override fun initView() {
         with(binding) {
             binding.viewModel = loginViewModel
             btnSignin.setOnClickListener {
                 findNavController().navigate(R.id.action_login_to_signup)
-            }
-            btnLogin.setOnClickListener {
-                startActivity(Intent(requireActivity(), MainActivity::class.java))
-                requireActivity().finish()
             }
         }
     }
