8a3d13512c6409c86d789405d9459be37d4965f3
lovejehan <jahan4707@naver.com>
2024-05-22 13:37:22 +0900

    feat(#71): RecommendViewModel 수정, Fragment CollectUiEvent 추가
    Co-authored-by: pooya98 <pooya98@naver.com>

--- Android/ninemova/app/src/main/java/com/ninemova/ui/recommend/RecommendFragment.kt
+++ Android/ninemova/app/src/main/java/com/ninemova/ui/recommend/RecommendFragment.kt
@@ -1,10 +1,16 @@
 package com.ninemova.ui.recommend
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.ninemova.R
 import com.ninemova.databinding.FragmentRecommendBinding
 import com.ninemova.ui.base.BaseFragment
+import com.ninemova.ui.dialog.LoadingDialog
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class RecommendFragment : BaseFragment<FragmentRecommendBinding>(R.layout.fragment_recommend) {
 
@@ -19,12 +25,37 @@ class RecommendFragment : BaseFragment<FragmentRecommendBinding>(R.layout.fragme
                 recommendVieModel.fetchNewWorldRecommendMovie()
             }
             ivRecommendMovieThumnail.setOnClickListener {
-                recommendVieModel.uiState.value.selectedMovie?.let {selectedMovie ->
+                recommendVieModel.uiState.value.selectedMovie?.let { selectedMovie ->
                     findNavController().navigate(
                         RecommendFragmentDirections.actionRecommendToDetail(selectedMovie)
                     )
                 }
             }
         }
+        collectUiEvent()
+        showLoadingDialog()
+    }
+
+    private fun collectUiEvent() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                recommendVieModel.uiEvent.collectLatest { event ->
+                    when (event) {
+                        is RecommendViewEvent.SearchSuccess -> {
+                            recommendVieModel.fetchAiRecommendMovie()
+                        }
+
+                        is RecommendViewEvent.Error -> {
+                            showMessage(event.errorMessage)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showLoadingDialog() {
+        val dialog = LoadingDialog(recommendVieModel.isLoading)
+        dialog.show(requireActivity().supportFragmentManager, "loadingDialog")
     }
 }
